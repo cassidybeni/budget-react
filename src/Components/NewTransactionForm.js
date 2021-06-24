69a9c80735e7a12ea14940d56f110b785f873729
cassidybeni <cassidybeni@pursuit.org>
2021-06-24 16:50:51 -0400

    Form is styled and responsive

--- src/Components/NewTransactionForm.js
+++ src/Components/NewTransactionForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router";
+import "./NewTransactionForm.css";
 
 function NewTransactionForm(props) {
   const [transaction, setTransactions] = useState({
@@ -16,45 +17,47 @@ function NewTransactionForm(props) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Date:
-        <input
-          type="text"
-          placeholder="date"
-          onChange={(e) => setTransactions(e.target.value)}
-          value={transaction.date}
-        ></input>
-      </label>
-      <label>
-        Name:
-        <input
-          type="text"
-          placeholder="name"
-          onChange={(e) => setTransactions(e.target.value)}
-          value={transaction.name}
-        ></input>
-      </label>
-      <label>
-        Amount:
-        <input
-          type="number"
-          placeholder="amount"
-          onChange={(e) => setTransactions(Number(e.target.value))}
-          value={transaction.amount}
-        ></input>
-      </label>
-      <label>
-        From:
-        <input
-          type="text"
-          placeholder="from"
-          onChange={(e) => setTransactions(e.target.value)}
-          value={transaction.from}
-        ></input>
-      </label>
-      <input type="submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Date:
+          <input
+            type="text"
+            placeholder="date"
+            onChange={(e) => setTransactions(e.target.value)}
+            value={transaction.date}
+          ></input>
+        </label>
+        <label>
+          Name:
+          <input
+            type="text"
+            placeholder="name"
+            onChange={(e) => setTransactions(e.target.value)}
+            value={transaction.name}
+          ></input>
+        </label>
+        <label>
+          Amount:
+          <input
+            type="number"
+            placeholder="amount"
+            onChange={(e) => setTransactions(Number(e.target.value))}
+            value={transaction.amount}
+          ></input>
+        </label>
+        <label>
+          From:
+          <input
+            type="text"
+            placeholder="from"
+            onChange={(e) => setTransactions(e.target.value)}
+            value={transaction.from}
+          ></input>
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
 
